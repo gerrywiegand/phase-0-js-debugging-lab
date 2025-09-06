b27e22512d8dee953d959d4c03cb87cea1a4f0d7
Gerry <gerrywiegand@gmail.com>
2025-09-05 21:15:55 -0400

    debug

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,19 +1,20 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
-    
+    debugger
     let totalPrice = 0;
     debugger
     for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+debugger
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
-
+debugger
     return totalPrice;
 }
+debugger
 let result = calculateDiscountedPrice(5, 20)
 console.log (result)
-
+debugger
 
 module.exports = calculateDiscountedPrice;
